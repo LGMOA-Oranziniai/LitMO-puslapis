8473d0b23707d003dad50abdc82ed8eb2c13670c
Dominykas Žviedrys <dominykaszviedrys@gmail.com>
2025-08-24 19:02:18 +0300

    Pradinis variantas

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Global constants file. Import by using the `import` keyword.
+
+export const SITE_TITLE = 'Astro Blog';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const BASE_URL = 'https://litmo.lgmoa.lt';
